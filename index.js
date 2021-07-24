edbd6da7caeb694a994f5f520bcb2dc4cd0b97b2
oMatheusmol <80729568+oMatheusmol@users.noreply.github.com>
2021-07-24 13:38:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+exports.greet = void 0;
+function greet(name) {
+    return "Ola " + name;
+}
+exports.greet = greet;
+console.log(greet('Mahteus Mol'));
---
+++ index.ts
@@ -0,0 +1,5 @@
+export function greet(name:string):string {
+    return `Ola ${name}`;
+}
+
+console.log(greet('Mahteus Mol'));
